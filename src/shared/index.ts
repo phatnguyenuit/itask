be91e7e22f28466877f9b6700faa438e4da48b36
Nguyen Phat <phatnt.uit@gmail.com>
2021-11-30 12:12:44 +0700

    feat: add apollo graphql hooks generator

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,6 +1,8 @@
 // THIS FILE IS GENERATED, DO NOT EDIT!!!
 import { GraphQLResolveInfo } from 'graphql';
 import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw';
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = {
@@ -15,6 +17,7 @@ export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
 export type RequireFields<T, K extends keyof T> = {
   [X in Exclude<keyof T, K>]?: T[X];
 } & { [P in K]-?: NonNullable<T[P]> };
+const defaultOptions = {};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -301,3 +304,117 @@ export const mockSearchTodosQuery = (
     'searchTodos',
     resolver,
   );
+
+export const GetBooksDocument = gql`
+  query getBooks {
+    getBooks {
+      title
+      author
+    }
+  }
+`;
+
+/**
+ * __useGetBooksQuery__
+ *
+ * To run a query within a React component, call `useGetBooksQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetBooksQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetBooksQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetBooksQuery(
+  baseOptions?: Apollo.QueryHookOptions<GetBooksQuery, GetBooksQueryVariables>,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<GetBooksQuery, GetBooksQueryVariables>(
+    GetBooksDocument,
+    options,
+  );
+}
+export function useGetBooksLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    GetBooksQuery,
+    GetBooksQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<GetBooksQuery, GetBooksQueryVariables>(
+    GetBooksDocument,
+    options,
+  );
+}
+export type GetBooksQueryHookResult = ReturnType<typeof useGetBooksQuery>;
+export type GetBooksLazyQueryHookResult = ReturnType<
+  typeof useGetBooksLazyQuery
+>;
+export type GetBooksQueryResult = Apollo.QueryResult<
+  GetBooksQuery,
+  GetBooksQueryVariables
+>;
+export const SearchTodosDocument = gql`
+  query searchTodos($userId: Int!, $searchParams: SearchTodoParams) {
+    searchTodos(userId: $userId, searchParams: $searchParams) {
+      id
+      userId
+      title
+      completed
+    }
+  }
+`;
+
+/**
+ * __useSearchTodosQuery__
+ *
+ * To run a query within a React component, call `useSearchTodosQuery` and pass it any options that fit your needs.
+ * When your component renders, `useSearchTodosQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useSearchTodosQuery({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *      searchParams: // value for 'searchParams'
+ *   },
+ * });
+ */
+export function useSearchTodosQuery(
+  baseOptions: Apollo.QueryHookOptions<
+    SearchTodosQuery,
+    SearchTodosQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<SearchTodosQuery, SearchTodosQueryVariables>(
+    SearchTodosDocument,
+    options,
+  );
+}
+export function useSearchTodosLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    SearchTodosQuery,
+    SearchTodosQueryVariables
+  >,
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<SearchTodosQuery, SearchTodosQueryVariables>(
+    SearchTodosDocument,
+    options,
+  );
+}
+export type SearchTodosQueryHookResult = ReturnType<typeof useSearchTodosQuery>;
+export type SearchTodosLazyQueryHookResult = ReturnType<
+  typeof useSearchTodosLazyQuery
+>;
+export type SearchTodosQueryResult = Apollo.QueryResult<
+  SearchTodosQuery,
+  SearchTodosQueryVariables
+>;
